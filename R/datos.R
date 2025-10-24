a60c9d7babec0b874e2042e3a7cc65a7fa992a43
Pao Corrales <paobcorrales@gmail.com>
2025-10-25 00:20:22 +1100

    Documentacionde datos

---
+++ R/datos.R
@@ -0,0 +1,10 @@
+#' Datos aleatorios
+#'
+#' Set de datos de ejemplo con datos aleatorios
+#'
+#' @format vector de largo 1000.
+#'
+#' \describe{
+#'  \item{datos}{numérico, de largo 1000.}
+#'}
+"datos"
