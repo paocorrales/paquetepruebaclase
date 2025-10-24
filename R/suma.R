bade93f8a1f4134baaa7a5e611130d135173c1d7
Pao Corrales <paobcorrales@gmail.com>
2025-10-25 00:20:07 +1100

    Agrega documentacion de suma

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,18 @@
+#' Suma dos números
+#'
+#' Suma dos números que deben ser positivos.
+#'
+#' @param x un valor o vector numérico.
+#' @param y un valor o vector numérico.
+#'
+#' @returns
+#' Un valor o vector numérico con la suma de x más y.
+#'
+#' @examples
+#' suma(10, 2)
+#' suma(1, 1)
+#'
+#' @export
 suma <- function(x, y) {
   x + y
 }
