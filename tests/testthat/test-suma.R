4067e261149b5809d23671f56d12aa459ee43319
Pao Corrales <paobcorrales@gmail.com>
2025-10-31 23:58:01 +1100

    Agrega tests para suma

---
+++ tests/testthat/test-suma.R
@@ -0,0 +1,11 @@
+test_that("la suma funciona", {
+
+  expect_equal(object = suma(2, 2), expected = 4)
+
+})
+
+test_that("no suma caracteres",{
+
+ expect_error(suma("1", 1), "El argumento x no es númerico")
+
+})
